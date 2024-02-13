9bb082057a0b62d174081a2ce4a063538ed0d7c7
Sam Allen <samallen659@gmail.com>
2024-02-13 20:03:35 +0000

    adds comments for header struct

--- internal/message/header.go
+++ internal/message/header.go
@@ -51,10 +51,10 @@ type Header struct {
 	// A four bit field that specifies kind of query in this
 	// message.  This value is set by the originator of a query
 	// and copied into the response.  The values are:
-	// 0               a standard query (QUERY)
-	// 1               an inverse query (IQUERY)
-	// 2               a server status request (STATUS)
-	// 3 to 15         reserved for future use
+	// 0               - a standard query (QUERY)
+	// 1               - an inverse query (IQUERY)
+	// 2               - a server status request (STATUS)
+	// 3 to 15         - reserved for future use
 	OPCODE OPCODE
 
 	// Authoritative Answer - this bit is valid in responses,
@@ -68,11 +68,51 @@ type Header struct {
 	// transmission channel.
 	TC byte
 
-	RD      byte
-	Z       byte
-	RC      RCODE
+	// Recursion Desired - If set, it directs the name server to
+	// pursue the query recursively
+	RD byte
+
+	// Reserved for future use
+	Z byte
+
+	// Response code - this 4 bit field is set as part of
+	// responses.  The values have the following
+	// interpretation:
+	// 0               - No error condition
+	// 1               - Format error - The name server was
+	//                 unable to interpret the query.
+	// 2               - Server failure - The name server was
+	//                 unable to process this query due to a
+	//                 problem with the name server.
+	// 3               - Name Error - Meaningful only for
+	//                 responses from an authoritative name
+	//                 server, this code signifies that the
+	//                 domain name referenced in the query does
+	//                 not exist.
+	// 4               - Not Implemented - The name server does
+	//                 not support the requested kind of query.
+	// 5               - Refused - The name server refuses to
+	//                 perform the specified operation for
+	//                 policy reasons.  For example, a name
+	//                 server may not wish to provide the
+	//                 information to the particular requester,
+	//                 or a name server may not wish to perform
+	//                 a particular operation (e.g., zone
+	RC RCODE
+
+	// An unsigned 16 bit integer specifying the number of entries
+	// int the question section.
 	QDCOUNT uint16
+
+	// An unsigned 16 bit integer specifying the number of resource
+	// records in the answer section.
 	ANCOUNT uint16
+
+	// An unsigned 16 bit integer specifying the number of name
+	// server resource records in the authroity records section.
 	NSCOUNT uint16
+
+	// An unsigned 16 biy integer specifying the number of resource
+	// records in the additional records section.
 	ARCOUNT uint16
 }
