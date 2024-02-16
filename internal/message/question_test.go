58ae1970e7aa915b8517d443de3e4ecf6dca6348
Sam Allen <samallen659@gmail.com>
2024-02-16 19:05:03 +0000

    adds test for question marshall method

--- internal/message/question_test.go
+++ internal/message/question_test.go
@@ -15,4 +15,29 @@ func TestQuestion(t *testing.T) {
 			t.Error("incorret byte slice received")
 		}
 	})
+	t.Run("Marshall returns correct byte slice of question", func(t *testing.T) {
+		qname := message.ConvertHostnameToQName("www.google.com")
+		question := message.Question{
+			QName:  qname,
+			QType:  message.QTYPE_A,
+			QClass: message.QCLASS_IN,
+		}
+		b := question.Marshall()
+
+		if len(b) != 20 {
+			t.Fatal("Incorrect length byte slice received")
+		}
+
+		if a := slices.Compare(b[:16], []byte{3, 119, 119, 119, 6, 103, 111, 111, 103, 108, 101, 3, 99, 111, 109, 0}); a != 0 {
+			t.Error("Incorrect QName revieced")
+		}
+
+		if a := slices.Compare(b[16:18], []byte{0, 1}); a != 0 {
+			t.Error("Incorrect QType recieved")
+		}
+
+		if a := slices.Compare(b[18:], []byte{0, 1}); a != 0 {
+			t.Error("Incorrect QClass received")
+		}
+	})
 }
