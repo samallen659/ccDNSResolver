bc0c9cb3ad8bd95c3229f3a06200ecbb34b0661e
Sam Allen <samallen659@gmail.com>
2024-02-19 19:53:05 +0000

    adds client Resolve method message is sent and response received and read into byte slice

--- internal/connection/client.go
+++ internal/connection/client.go
@@ -3,7 +3,9 @@ package connection
 import (
 	"errors"
 	"fmt"
+	"github.com/samallen659/ccDNSResolver/internal/message"
 	"net"
+	"time"
 )
 
 type Client struct {
@@ -21,4 +23,48 @@ func NewClient(addr string) (*Client, error) {
 	}
 
 	return &Client{addr: udpAddr}, nil
+}
+
+func (c *Client) Resolve(hostname string) error {
+
+	id := message.NewHeaderID()
+	m := message.Message{
+		Header: message.Header{
+			ID:      id,
+			QR:      byte(0),
+			OPCode:  message.OPCODEQuery,
+			RD:      byte(1),
+			QDCOUNT: uint16(1),
+		},
+		Question: message.Question{
+			QName:  message.ConvertHostnameToQName(hostname),
+			QType:  message.QTYPE_A,
+			QClass: message.QCLASS_IN,
+		}}
+
+	mBytes := m.Marshall()
+	fmt.Println(mBytes)
+
+	fmt.Println("setting up connection")
+	conn, err := net.DialUDP("udp", nil, c.addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+	_, err = conn.Write(mBytes)
+	if err != nil {
+		return err
+	}
+
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(buf)
+
+	return nil
 }
