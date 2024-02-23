24b12bf386cfc90725b347242bedf1224be3bb9e
Sam Allen <samallen659@gmail.com>
2024-02-23 14:54:45 +0000

    adds comments to ResourceRecord

--- internal/message/ResourceRecord.go
+++ internal/message/ResourceRecord.go
@@ -1,10 +1,25 @@
 package message
 
 type ResourceRecord struct {
-	Name     []byte
-	Type     uint16
-	Class    uint16
-	TTL      uint32
+	// The domain name this resorce record pertains
+	Name []byte
+
+	// Contains one of the RR type codes. This field
+	// specifies the meaning of the data in RData
+	Type uint16
+
+	// Specifies the class of the data in RData
+	Class uint16
+
+	// Specifies the time interval (in seconds) that the
+	// resource record may be cached before it should be
+	// discarded. Zero values mean it should not be cached
+	TTL uint32
+
+	// Specifies the length in bytes of RData
 	RDLength uint16
-	RData    []byte
+
+	// Describes the resource depending of the Type and Class
+	// fields
+	RData []byte
 }
