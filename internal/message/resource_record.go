24450596e4eae7e9087a4ed6d2437de53ebdf082
Sam Allen <samallen659@gmail.com>
2024-02-23 15:23:59 +0000

    adds CLASS and TYPE types for DNS response

--- internal/message/resource_record.go
+++ internal/message/resource_record.go
@@ -1,5 +1,35 @@
 package message
 
+type TYPE uint16
+
+const (
+	TYPE_A     TYPE = 1
+	TYPE_NS    TYPE = 2
+	TYPE_MD    TYPE = 3
+	TYPE_MF    TYPE = 4
+	TYPE_CNAME TYPE = 5
+	TYPE_SOA   TYPE = 6
+	TYPE_MB    TYPE = 7
+	TYPE_MG    TYPE = 8
+	TYPE_MR    TYPE = 9
+	TYPE_NULL  TYPE = 10
+	TYPE_WKS   TYPE = 11
+	TYPE_PTR   TYPE = 12
+	TYPE_HINFO TYPE = 13
+	TYPE_MINFO TYPE = 14
+	TYPE_MX    TYPE = 15
+	TYPE_TXT   TYPE = 16
+)
+
+type CLASS uint16
+
+const (
+	CLASS_IN CLASS = 1
+	CLASS_CS CLASS = 2
+	CLASS_CH CLASS = 3
+	CLASS_HS CLASS = 4
+)
+
 type ResourceRecord struct {
 	// The domain name this resorce record pertains
 	Name []byte
